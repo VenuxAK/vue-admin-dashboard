355d0d5ca63e3fbe4e4f34e6ceed63454b7a65ec
VenuxAK <shelby.ak68@gmail.com>
2025-10-06 07:36:53 +0000

    initial commit

---
+++ src/views/IndexView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold text-green-500 underline">Hello world!</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
